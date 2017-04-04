4cf79eb7821bd2b118e0216fbe848a7eaa1e5c59
n0f3 <n0f3@outlook.com>
2017-04-03 18:31:58 -0700

    Added initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="panel panel-default">
+      <div className="panel-heading text-center" >
+        <h1>FreeCodeCamp Recipes box</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
